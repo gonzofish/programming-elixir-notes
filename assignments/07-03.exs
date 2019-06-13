76cf31500e5ea120a380c8eaade56132ea96b0a9
Matt Fehskens <matt.fehskens@gmail.com>
2019-06-12 21:48:25 -0400

    Answer for ch 7, assignment 3

---
+++ assignments/07-03.exs
@@ -0,0 +1,9 @@
+defmodule Codex do
+  def caesar([], _), do: []
+  def caesar([head | tail], n) when head + n > ?z do
+    [(?a - 1) + ((head + n) - ?z) | caesar(tail, n)]
+  end
+  def caesar([head | tail], n) do
+    [head + n | caesar(tail, n)]
+  end
+end
