5898244e85d1b801e748e8d87f7f57546cd1ab3c
Matt Fehskens <matt.fehskens@gmail.com>
2019-06-14 16:45:39 -0400

    Implement .filter for ch 10, assignment 1

--- assignments/10-01.exs
+++ assignments/10-01.exs
@@ -12,4 +12,13 @@ defmodule MEnum do
     func.(head)
     each(tail, func)
   end
+
+  def filter([], _), do: []
+  def filter([head | tail], func) do
+    if func.(head) do
+      [head | filter(tail, func)]
+    else
+      filter(tail, func)
+    end
+  end
 end
