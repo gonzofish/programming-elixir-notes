66f92a111c3e3ed2bd55cce21cacf90ee7982464
Matt Fehskens <matt.fehskens@gmail.com>
2019-07-05 22:41:29 -0400

    Add Poison to project andprocess value returned from GithubIssues

--- issues/lib/github_issues.ex
+++ issues/lib/github_issues.ex
@@ -11,11 +11,13 @@ defmodule GithubIssues do
     "https://api.github.com/repos/#{user}/#{project}/issues"
   end
 
-  defp handle_response({:ok, %{status_code: 200, body: body}}) do
-    {:ok, body}
+  defp handle_response({_, %{status_code: status_code, body: body}}) do
+    {
+      status_code |> check_for_error(),
+      body |> Poison.Parser.parse!()
+    }
   end
 
-  defp handle_response({_, %{status_code: _, body: body}}) do
-    {:error, body}
-  end
+  defp check_for_error(200), do: :ok
+  defp check_for_error(_), do: :error
 end
