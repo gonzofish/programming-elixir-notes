36740bbbafbb35c7a948c46c3d828fc10bd7e0b4
Matt Fehskens <matt.fehskens@gmail.com>
2019-07-17 16:22:09 -0400

    Updated 15-02 to spawn the processes

--- assignments/15/15-02.exs
+++ assignments/15/15-02.exs
@@ -14,29 +14,29 @@ defmodule Neeq do
   def listen do
     receive do
       token ->
-        "Received #{token} back"
+        IO.puts("Received #{token} back")
     after
       1000 ->
-        "Game over..."
+        IO.puts("Game over...")
     end
   end
 end
 
+fred_pid = spawn(Neeq, :listen_and_send, [])
+betty_pid = spawn(Neeq, :listen_and_send, [])
+
+send(fred_pid, {self(), :fred})
+Neeq.listen()
+
+send(betty_pid, {self(), :betty})
+Neeq.listen()
+
+Neeq.listen()
+
 """
 iex(1)> c "15-02.exs"
+Received fred back
+Received betty back
+Game over...
 [Neeq]
-iex(2)> fred_pid = spawn(Neeq, :listen_and_send, [])
-#PID<0.115.0>
-iex(3)> betty_pid = spawn(Neeq, :listen_and_send, [])
-#PID<0.117.0>
-iex(4)> send(fred_pid, {self(), :fred})
-{#PID<0.108.0>, :fred}
-iex(5)> send(betty_pid, {self(), :betty})
-{#PID<0.108.0>, :betty}
-iex(6)> Neeq.listen()
-"Received fred back"
-iex(7)> Neeq.listen()
-"Received betty back"
-iex(8)> Neeq.listen()
-"Game over..."
 """
